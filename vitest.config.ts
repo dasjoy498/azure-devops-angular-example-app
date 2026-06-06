37c7a1712be720bb9d29cc8980c6e63d846106b9
Joy <dasjoy498@gmail.com>
2026-06-06 12:34:05 +0530

    chore(app): configure azure pipelines and code coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,8 +5,9 @@ const config: UserConfig = {
     environment: 'jsdom',
     setupFiles: ['vitest-setup.config.ts'],
     coverage: {
+      provider: 'istanbul', // Force Istanbul natively
       reportsDirectory: 'coverage',
-      reporter: ['text', 'lcov', 'cobertura'], // Cobertura for Azure DevOps
+      reporter: ['text', 'lcov', 'cobertura'], // Native built-in formats
       exclude: ['**/*.html'],
     },
     globals: true,
