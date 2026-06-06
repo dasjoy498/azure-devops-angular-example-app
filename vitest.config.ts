cc63dae30a1a2d3d2bf2b9412c10aed08dc4f19d
Joy <dasjoy498@gmail.com>
2026-06-06 10:18:03 +0530

    Updated the coverage report format to cobertura.

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,16 @@
-// Learn more about Vitest configuration options at https://vitest.dev/config/
-import { defineConfig } from 'vitest/config';
+import type { UserConfig } from 'vitest/config';
 
-export default defineConfig({
+const config: UserConfig = {
   test: {
     environment: 'jsdom',
     setupFiles: ['vitest-setup.config.ts'],
     coverage: {
       reportsDirectory: 'coverage',
+      reporter: ['text', 'lcov', 'cobertura'], // Cobertura for Azure DevOps
       exclude: ['**/*.html'],
     },
     globals: true,
   },
-});
+};
+
+export default config;
